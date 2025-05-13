39aea671a0856fcff0157012b0fa0eb053f4fb52
musharrafhamraz <musharrafhamraz02@gmail.com>
2025-05-13 15:46:59 +0500

    updated readme file.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6dcd7402f5b807e0"; }
+/******/ 		__webpack_require__.h = function() { return "10c9313e64552495"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
